2fd172fa8bfec7ab3a8763a0dda9820ea8e685fa
Oscar Canedo <oscar.canedo@bootcamp-upgrade.com>
2022-04-14 00:19:10 +0200

    First Iteration

---
+++ index.js
@@ -0,0 +1,6 @@
+// 1.Crea una función que tome como parámetro un número. Si es entero, mostramos true / false
+
+function isInterger(num) {
+    return Number.isInteger(num);
+}
+console.log(isInterger(10.5));
