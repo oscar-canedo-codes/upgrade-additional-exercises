37c781c0816e73a6fa6ec0271354da014d10654e
Oscar Canedo <oscar.canedo@bootcamp-upgrade.com>
2022-04-14 00:51:32 +0200

    Iteration #2

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ function isInterger(num) {
     return Number.isInteger(num);
 }
 console.log(isInterger(10.5));
+
+//2. Crea una función que tome como parámetros dos strings. Si a contiene b, b + a. Si no, añade b al final. Por ejemplo:
+
+//(“cheese”, “cake”) => output: “cheesecake”
+/* (“Java”, “script”) => output: “JavaScript”
+(“think, therefore I am”, “I”) => output: “I think, therefore I am” */
+
+function wordsTogether(a,b) {
+    if (a.includes(b)) {
+        return b + " " + a;
+    } else {
+        return a + b;
+    }
+}
+console.log(wordsTogether("cheese", "cake"));
